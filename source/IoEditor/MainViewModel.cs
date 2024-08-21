e107d1deaae8b83f54fbe3b4a7a02da6bb45c6e8
Andor Schindler <schindler.andor@nexogen.hu>
2024-08-21 19:29:23 +0200

    Initial import

---
+++ source/IoEditor/MainViewModel.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
+using IoEditor.UI.LoaderWindow;
+using IoEditor.UI.Utils;
+
+namespace IoEditor
+{
+    internal class MainViewModel : INotifyPropertyChanged
+    {
+        public ICommand OpenFilesCommand { get; }
+        public ICommand SaveFileCommand { get; }
+        public ICommand ExitCommand { get; }
+
+        public MainViewModel()
+        {
+            OpenFilesCommand = new DelegateCommand(OpenFilesCmd);
+            SaveFileCommand = new DelegateCommand(SaveFileCmd);
+            ExitCommand = new DelegateCommand(ExitCmd);
+        }
+
+        private void ExitCmd(object obj)
+        {
+            Application.Current.Shutdown();
+        }
+
+        private void SaveFileCmd(object obj)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void OpenFilesCmd(object obj)
+        {
+            var vm = new LoaderViewModel();
+            var win = new LoaderWindow() { DataContext = vm };
+            var dr = win.ShowDialog();
+
+            if (dr == true)
+            {
+                Console.WriteLine(vm.ReferenceFile);
+                Console.WriteLine(vm.TargetFile);
+            }
+        }
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        private void RaisePropertyChanged(string propertyName)
+            => PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+    }
+}
---
+++ source/IoEditor/UI/LoaderWindow/LoaderViewModel.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using IoEditor.UI.Utils;
+using Microsoft.Win32;
+using System.Windows.Input;
+using System.IO;
+using System.Windows;
+using System.Reflection.Metadata;
+
+namespace IoEditor.UI.LoaderWindow
+{
+    internal class LoaderViewModel : INotifyPropertyChanged
+    {
+        private string _referenceFile;
+        public string ReferenceFile
+        {
+            get { return _referenceFile; }
+            set
+            {
+                if (_referenceFile != value)
+                {
+                    _referenceFile = value;
+                    RaisePropertyChanged(nameof(ReferenceFile));
+                }
+            }
+        }
+
+        private string _targetFile;
+        public string TargetFile
+        {
+            get { return _targetFile; }
+            set
+            {
+                if (_targetFile != value)
+                {
+                    _targetFile = value;
+                    RaisePropertyChanged(nameof(TargetFile));
+                }
+            }
+        }
+
+        public ICommand OpenCommand { get; }
+        public ICommand CancelCommand { get; }
+        public ICommand BrowseReferenceFileCommand { get; }
+        public ICommand BrowseTargetFileCommand { get; }
+
+        public LoaderViewModel()
+        {
+            OpenCommand = new DelegateCommand(Open);
+            CancelCommand = new DelegateCommand(Cancel);
+            BrowseReferenceFileCommand = new DelegateCommand(BrowseReferenceFile);
+            BrowseTargetFileCommand = new DelegateCommand(BrowseTargetFile);
+        }
+
+        private void Open(object param)
+        {
+            if (!File.Exists(ReferenceFile))
+            {
+                MessageBox.Show($"Reference file does not exist: {ReferenceFile}", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+                return;
+            }
+
+            if (!File.Exists(TargetFile))
+            {
+                MessageBox.Show($"Target file does not exist: {TargetFile}", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+                return;
+            }
+
+            if (ReferenceFile == TargetFile)
+            {
+                MessageBox.Show("Reference file and Target file cannot be the same.", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+                return;
+            }
+
+            // Set the DialogResult to true and close the window
+            if (param is Window window)
+            {
+                window.DialogResult = true;
+                window.Close();
+            }
+        }
+
+        private void Cancel(object param)
+        {
+            // Close the window without setting DialogResult
+            if (param is Window window)
+            {
+                window.DialogResult = false;
+                window.Close();
+            }
+        }
+
+        private void BrowseReferenceFile(object param)
+        {
+            var openFileDialog = new OpenFileDialog();
+            if (openFileDialog.ShowDialog() == true)
+            {
+                ReferenceFile = openFileDialog.FileName;
+            }
+        }
+
+        private void BrowseTargetFile(object param)
+        {
+            var openFileDialog = new OpenFileDialog();
+            if (openFileDialog.ShowDialog() == true)
+            {
+                TargetFile = openFileDialog.FileName;
+            }
+        }
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        private void RaisePropertyChanged(string propertyName)
+            => PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+    }
+}
